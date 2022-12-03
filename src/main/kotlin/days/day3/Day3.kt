0508b061c039835bcf06f0453a7225d54699768a
ckainz11 <christoph.kainz11@gmail.com>
2022-12-04 00:48:14 +0100

    refactor day 3

--- src/main/kotlin/days/day3/Day3.kt
+++ src/main/kotlin/days/day3/Day3.kt
@@ -4,16 +4,15 @@ import days.Day
 
 object Day3 : Day {
     override fun solve1(input: String): String = input.lines()
-        .map { it.subSequence(0 until it.length / 2).toSet().intersect(it.subSequence(it.length / 2 until it.length).toSet()) }
-        .sumOf { shared -> shared.sumOf { item -> item.priority() } }
+        .map { it.chunked(it.length / 2) }
+        .sumOf { it.sharedItem().priority() }
         .toString()
 
 
     override fun solve2(input: String): String = input.lines().chunked(3)
-        .map { Pair(it[0].toSet().intersect(it[1].toSet()), it[2]) }
-        .map { it.first.intersect(it.second.toSet()) }
-        .sumOf { it.first().priority() }
+        .sumOf { it.sharedItem().priority() }
         .toString()
     private fun Char.priority(): Int = 1 + if (isUpperCase()) this - 'A' + 26 else this - 'a'
+    private fun List<String>.sharedItem(): Char = map { it.toSet() }.reduce { left, right -> left intersect right}.first()
 
 }
