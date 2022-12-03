6045b9d52c09fb4d77251324e3c645668470e4f4
ckainz11 <christoph.kainz11@gmail.com>
2022-12-04 00:37:39 +0100

    solve day 3

--- src/main/kotlin/days/day3/Day3.kt
+++ src/main/kotlin/days/day3/Day3.kt
@@ -5,13 +5,15 @@ import days.Day
 object Day3 : Day {
     override fun solve1(input: String): String = input.lines()
         .map { it.subSequence(0 until it.length / 2).toSet().intersect(it.subSequence(it.length / 2 until it.length).toSet()) }
-        .sumOf { shared -> shared.sumOf { item -> (item - 'a' + 1).let { if (it < 0) 58 + it else it } } }
+        .sumOf { shared -> shared.sumOf { item -> item.priority() } }
         .toString()
 
 
     override fun solve2(input: String): String = input.lines().chunked(3)
-        .map { Pair(it[0].toList().intersect(it[1].toSet()), it[2]) }
-        .map { pair -> pair.first.intersect(pair.second.toSet()) }
-        .sumOf { badge -> (badge.first() - 'a' + 1).let { if (it < 0) 58 + it else it } }
+        .map { Pair(it[0].toSet().intersect(it[1].toSet()), it[2]) }
+        .map { it.first.intersect(it.second.toSet()) }
+        .sumOf { it.first().priority() }
         .toString()
+    private fun Char.priority(): Int = 1 + if (isUpperCase()) this - 'A' + 26 else this - 'a'
+
 }
