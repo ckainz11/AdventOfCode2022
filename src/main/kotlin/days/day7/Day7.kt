34aeac4e3f35b015da3bf826b621dee97b1e678f
Christoph Kainz <ckainz11@users.noreply.github.com>
2022-12-07 18:49:07 +0100

    refactor day 7

--- src/main/kotlin/days/day7/Day7.kt
+++ src/main/kotlin/days/day7/Day7.kt
@@ -3,56 +3,35 @@ package days.day7
 import days.Day
 
 class Day7(override var input: String) : Day(input) {
-    private fun parseSizes(lines: List<String>): MutableList<Dir> {
-        val root = Dir("/")
-        val dirs = mutableListOf(root)
-        var cur = root
 
-        lines.drop(1).forEach {
+    private var directories = emptyList<Directory>()
+
+    private fun parseSizes(lines: List<String>): MutableList<Directory> {
+        val directories = mutableListOf(Directory(null))
+        var current: Directory = directories.first()
+        lines.drop(1).forEach { line ->
             when {
-                it.startsWith("$ cd") -> {
-                    val name = it.drop(5)
-                    cur = if (name == "..") cur.parent!! else cur.children.getValue(name)
-                }
-
-                it.startsWith("dir") -> {
-                    val name = it.drop(4)
-                    val child = Dir(
-                        parent = cur,
-                        name = name
-                    )
-                    dirs.add(child)
-                    cur.children[name] = child
-                }
-
-                it.first().isDigit() -> {
-                    val toAdd = it.split(" ").first().toInt()
-                    cur.size += toAdd
-                    var p = cur.parent
-                    while (p != null) {
-                        p.size += toAdd
-                        p = p.parent
-                    }
-                }
+                line == "$ cd .." -> current = current.parent!!
+                line.startsWith("$ cd") -> Directory(current).also { current.children += it; directories += it; current = it }
+                line[0].isDigit() -> current.filesSize += line.substringBefore(" ").toLong()
             }
         }
-        return dirs
+        return directories
     }
 
     override fun solve1(): String {
-        return parseSizes(input.lines()).sumOf { if (it.size < 100000) it.size else 0 }.toString()
+        directories = parseSizes(input.lines())
+        return directories.sumOf { if (it.totalSize < 100_000) it.totalSize else 0 }.toString()
     }
 
     override fun solve2(): String {
-        val dirs = parseSizes(input.lines())
-        val missing = 70000000 - dirs.first().size
-        return dirs.filter { it.size >= 30000000 - missing }.minOf { it.size }.toString()
+        val missing = 30_000_000 - ( 70_000_000 - directories.first().totalSize)
+        return directories.filter { it.totalSize >= missing }.minOf { it.totalSize }.toString()
     }
 
-    class Dir(
-        val name: String,
-        val parent: Dir? = null,
-        val children: MutableMap<String, Dir> = mutableMapOf(),
-        var size: Int = 0
-    )
+    data class Directory(val parent: Directory?, var children: List<Directory> = emptyList()) {
+        val totalSize: Long get() = filesSize + children.sumOf { it.totalSize }
+        var filesSize = 0L
+    }
+
 }
