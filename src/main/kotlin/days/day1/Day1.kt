a7c4ea394e63bd2b8b3f8b2497a6987e881ea6b5
ckainz11 <christoph.kainz11@gmail.com>
2022-12-08 14:18:11 +0100

    refactor day 1

--- src/main/kotlin/days/day1/Day1.kt
+++ src/main/kotlin/days/day1/Day1.kt
@@ -4,11 +4,11 @@ import days.Day
 
 class Day1(override val input: String) : Day(input) {
     override fun solve1(): String =
-        input.split("\r\n\r\n").map { elves -> elves.lines().sumOf { it.toInt() } }.maxOf { calories -> calories }
+        input.split("\n\n").map { elves -> elves.lines().sumOf { it.toInt() } }.maxOf { calories -> calories }
             .toString()
 
     override fun solve2(): String =
-        input.split("\r\n\r\n").map { elves -> elves.lines().sumOf { it.toInt() } }.sortedDescending().take(3).sum()
+        input.split("\n\n").map { elves -> elves.lines().sumOf { it.toInt() } }.sortedDescending().take(3).sum()
             .toString()
 }
 
